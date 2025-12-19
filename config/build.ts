17226d83244e85831ae3639e56660c0f55d2a29f
ike-gg <67127723+ike-gg@users.noreply.github.com>
2025-12-19 12:17:35 +0100

    chore: removed background worker

--- config/build.ts
+++ config/build.ts
@@ -5,15 +5,12 @@ import manifest from "../public/manifest.json";
 
 const outdir = "./build";
 
-const {
-	content_scripts,
-	background: { service_worker },
-} = manifest;
+const { content_scripts } = manifest;
 
 const scripts = content_scripts.flatMap((script) => script.js);
 
 const resolveEntryPoints = (entrypoints: string[]) => {
-	return entrypoints.map((entrypoint) => `./src/${entrypoint}`);
+  return entrypoints.map((entrypoint) => `./src/${entrypoint}`);
 };
 
 const publicFolder = "./public";
@@ -21,19 +18,15 @@ const publicFolder = "./public";
 await $`rm -rf ${outdir}`;
 
 const ext = {
-	html: ".html",
-	png: ".png",
-	css: ".css",
+  html: ".html",
+  png: ".png",
+  css: ".css",
 };
 
 await Bun.build({
-	target: "browser",
-	entrypoints: resolveEntryPoints([
-		...scripts,
-		service_worker,
-		"popup/index.tsx",
-	]),
-	outdir,
+  target: "browser",
+  entrypoints: resolveEntryPoints([...scripts, "popup/index.tsx"]),
+  outdir,
 });
 
 const glob = new Glob("**");
@@ -43,22 +36,22 @@ const mainCssFile = Bun.file(`${publicFolder}/main.css`);
 if (!mainCssFile.exists()) throw new Error("main.css not found");
 
 for await (const filename of glob.scan(publicFolder)) {
-	const file = Bun.file(`${publicFolder}/${filename}`);
+  const file = Bun.file(`${publicFolder}/${filename}`);
 
-	if (!file.exists()) throw new Error(`File ${filename} does not exist`);
+  if (!file.exists()) throw new Error(`File ${filename} does not exist`);
 
-	if (filename.endsWith(ext.png) || filename.endsWith(ext.css)) continue;
+  if (filename.endsWith(ext.png) || filename.endsWith(ext.css)) continue;
 
-	if (filename.endsWith(ext.html)) {
-		const fileFolder = filename.replace(ext.html, "");
+  if (filename.endsWith(ext.html)) {
+    const fileFolder = filename.replace(ext.html, "");
 
-		// rename files to index.html since it's being copied into a folder that share its original name
-		await $`cp ${file.name} ${outdir}/${fileFolder}/index.html`;
-		// copy the css file into the folder
-		await $`bun run css -- ${mainCssFile.name} -o ${outdir}/${fileFolder}/main.css`.quiet();
-	} else {
-		await $`cp ${file.name} ${outdir}`;
-	}
+    // rename files to index.html since it's being copied into a folder that share its original name
+    await $`cp ${file.name} ${outdir}/${fileFolder}/index.html`;
+    // copy the css file into the folder
+    await $`bun run css -- ${mainCssFile.name} -o ${outdir}/${fileFolder}/main.css`.quiet();
+  } else {
+    await $`cp ${file.name} ${outdir}`;
+  }
 }
 
 await $`cp -R ${publicFolder}/icons ${outdir}`;
